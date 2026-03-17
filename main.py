e0def4637fd5cbab73adba5665e651d4142fc2b4
Mike Slayback <Scott.M.Slayback@gmail.com>
2026-03-17 11:16:24 -0700

    Point and Line classes added for drawing.

--- main.py
+++ main.py
@@ -1,5 +1,24 @@
 from tkinter import Tk, BOTH, Canvas
 
+class Point:
+	def __init__(self, x, y):
+		self.x = x
+		self.y = y
+
+class Line:
+	def __init__(self, pointA:Point, pointB:Point):
+		self.pointA = pointA
+		self.pointB = pointB
+	
+	def draw(self, canvas:Canvas, fill_color:str):
+		x1 = self.pointA.x
+		y1 = self.pointA.y
+		x2 = self.pointB.x
+		y2 = self.pointB.y
+
+		canvas.create_line(x1, y1, x2, y2, fill=fill_color, width = 2)
+
+
 class Window:
 	def __init__(self, width, height):
 		self.width = width
@@ -25,8 +44,25 @@ class Window:
 	def close(self):
 		self.is_running = False
 
+	def draw_line(self, line:Line, fill_color="black"):
+		line.draw(self.canvas, fill_color)
+
+
 def main():
 	win = Window(800, 600)
+	pointA = Point(10, 10)
+	pointB = Point(30, 30)
+	pointC = Point(10, 50)
+	pointD = Point(80, 10)
+	line1 = Line(pointA, pointB)
+	line2 = Line(pointA, pointC)
+	line3 = Line(pointA, pointD)
+	line4 = Line(pointB, pointD)
+	win.draw_line(line1, "black")
+	win.draw_line(line2)
+	win.draw_line(line3, "green")
+	win.draw_line(line4, "red")
+
 	win.wait_for_close()
 
 if __name__ == "__main__":
