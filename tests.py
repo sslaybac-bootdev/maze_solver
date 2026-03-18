372a52f98598d4f23c0f005fe78a13dae5f84213
Mike Slayback <Scott.M.Slayback@gmail.com>
2026-03-18 14:56:50 -0700

    Added tests for verifying that the entrance and exit are open.

--- tests.py
+++ tests.py
@@ -11,5 +11,14 @@ class Tests(unittest.TestCase):
 		self.assertEqual(num_cols, len(m1.cells))
 		self.assertEqual(num_rows, len(m1.cells[0]))
 
+	def test_open_entrance_and_exit(self):
+		num_cols = 12
+		num_rows = 10
+		NWpoint = Point(10, 10)
+		m1 = Maze(NWpoint, 50, num_rows, num_cols)
+		self.assertFalse(m1.cells[0][0].has_top_wall)
+		self.assertFalse(m1.cells[-1][-1].has_bottom_wall)
+
+
 if __name__ == "__main__":
 	unittest.main()
